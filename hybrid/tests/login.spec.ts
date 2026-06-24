588e94439b41090440ed7247678af8ad0f12be58
Mokkshith Poojari <moxith001@gmail.com>
2026-06-24 11:45:43 +0530

    2nd branch

--- hybrid/tests/login.spec.ts
+++ hybrid/tests/login.spec.ts
@@ -16,7 +16,6 @@ test('login with valid credentials', async ({ page }) => {
 test('login with Invalid credentials', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
-
   await loginPage.login('student', 'Password')
 
 });
